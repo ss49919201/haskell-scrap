063c4b6b59ed86cd463deac2650b7b410715bb04
s-beats <sakashin10291029@gmail.com>
2022-06-24 22:01:33 +0900

    feat: lambda

--- Lambda.hs
+++ Lambda.hs
@@ -3,14 +3,12 @@ import Control.Monad (unless, when)
 one :: Int
 one = 1
 
-true :: Bool
-true = True
+false :: Bool
+false = False
 
 main :: IO ()
 main = do
   let n = one
-  let t = true
+  let t = false
   (\x -> when (x == 0) $ print x) n -- 1
-  (\x -> when (x == 1) $ print x) n
-  (\y -> when y $ print y) t -- "True"
   (\y -> unless y $ print y) t
