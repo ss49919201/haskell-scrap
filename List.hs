504b7f971c624356443e341047ee7b7150427352
s-beats <sakashin10291029@gmail.com>
2022-06-24 22:37:28 +0900

    feat: list

---
+++ List.hs
@@ -0,0 +1,11 @@
+main = do
+  let nl = [0, 1, 2]
+  print (nl !! 1) -- 1
+  --   print (nl !! 3) -- List.hs: Prelude.!!: index too large
+  --   print (nl !! (-3)) -- List.hs: Prelude.!!: negative index
+
+  --   全ての要素に同じ処理
+  print [(+ 1 * 2) n | n <- nl] -- [2,3,4]
+
+  --   条件付きで取り出す
+  print [n | n <- nl, n `div` 2 == 0] -- [0,1]
