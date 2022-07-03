e9aff12e29c1e0683c3d49a8f714253e9a5d8104
s-beats <sakashin10291029@gmail.com>
2022-07-03 20:45:32 +0900

    feat: io

--- IO.hs
+++ IO.hs
@@ -1,7 +1,4 @@
-main = do
-  a
-  b
-  c
+import Control.Monad
 
 a = do
   [a, b] <- map read . words <$> getLine
@@ -15,3 +12,12 @@ c = do
   a <- getLine
   let fn = print . length . filter (== '1')
   fn a
+
+d = do
+  a <- readLn :: IO Int
+  print (a `div` 10)
+
+e = do
+  -- 222 -> ["222"]
+  -- require "import Control.Monad"
+  replicateM 1 getLine >>= print
