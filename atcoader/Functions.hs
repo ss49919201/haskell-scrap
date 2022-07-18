7794636edfedac3a0895d1cdf1d2855a11babbff
s-beats <sakashin10291029@gmail.com>
2022-07-19 08:38:36 +0900

    get line

--- atcoader/Functions.hs
+++ atcoader/Functions.hs
@@ -1,5 +1,6 @@
 main = do
-  print "Hello World"
+  l <- getLineToIntList
+  print l
 
 -- リストに、要素が含まれるかを返す。
 -- contains contains 1 [1 .. 10] は True になる。
@@ -16,3 +17,15 @@ getLineToString = do
 getLineToInt :: IO Int
 getLineToInt = do
   read <$> getLine :: IO Int
+
+-- 入力の1行を読み込んで、文字列のリストに変換する。
+-- 1 2 3 は ["1", "2", "3"] になる。
+getLineToStringList :: IO [String]
+getLineToStringList = do
+  words <$> getLine
+
+-- 入力の1行を読み込んで、数値のリストに変換する。
+-- 1 2 3 は [1, 2, 3] になる。
+getLineToIntList :: IO [Int]
+getLineToIntList = do
+  map read . words <$> getLine :: IO [Int]
