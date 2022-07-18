3c601bdcfcef29052da3f23360e0e2c0534f2df1
s-beats <sakashin10291029@gmail.com>
2022-07-19 08:33:18 +0900

    get line

--- atcoader/Functions.hs
+++ atcoader/Functions.hs
@@ -1,8 +1,18 @@
 main = do
-  print $ contains 1 [1 .. 10]
+  print "Hello World"
 
 -- リストに、要素が含まれるかを返す。
 -- contains contains 1 [1 .. 10] は True になる。
 contains :: Eq a => a -> [a] -> Bool
 contains x [] = False
 contains x ys = x `elem` ys
+
+-- 入力の1行を読み込んで、文字列に変換する。
+getLineToString :: IO String
+getLineToString = do
+  getLine
+
+-- 入力の1行を読み込んで、数値に変換する。
+getLineToInt :: IO Int
+getLineToInt = do
+  read <$> getLine :: IO Int
