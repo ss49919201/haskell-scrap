0b30d04de997eb09d6af17cee7a681595691ddff
s-beats <sakashin10291029@gmail.com>
2022-07-24 10:42:55 +0900

    cosure

--- Closure.hs
+++ Closure.hs
@@ -1,3 +1,24 @@
-counter = (\_ -> val + 1)
+counter :: p -> Integer
+counter = f
   where
     val = 0
+    f = (\_ -> val + 1)
+
+addInt :: Num a => a -> a -> a
+addInt n = f
+  where
+    f = (n +)
+
+main = do
+  let f = counter
+  let a = f 1
+  let b = f 1
+  print a
+  print b
+
+  let f2 = addInt 1
+  let c = f2 2 -- 2 + 1 = 3
+  print c -- 3
+  let f3 = addInt 2
+  let d = f3 2 -- 2 + 2 = 4
+  print d -- 4
