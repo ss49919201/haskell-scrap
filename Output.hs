c747c94aa834916143eb641aac1378469f655a35
s-beats <sakashin10291029@gmail.com>
2022-07-26 22:18:33 +0900

    別の書き方をコメント

--- Output.hs
+++ Output.hs
@@ -13,5 +13,7 @@ main = do
   -- リストを文字列変換して出力
   let z = [1 .. 3]
   let z' = ["a", "b", "c"] :: [String]
+  --   (putStrLn . unwords) (map show z) とも書ける
   putStrLn . unwords $ map show z -- 1 2 3
+  --   (putStrLn . unwords) z' とも書ける
   putStrLn . unwords $ z' -- a b c
