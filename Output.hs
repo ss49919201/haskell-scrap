4570224ab5a8926d4bef046073a41637f574de7a
s-beats <sakashin10291029@gmail.com>
2022-07-26 22:18:20 +0900

    出力結果をコメント

--- Output.hs
+++ Output.hs
@@ -10,7 +10,8 @@ main = do
   let y = 1.5
   print y
 
+  -- リストを文字列変換して出力
   let z = [1 .. 3]
   let z' = ["a", "b", "c"] :: [String]
-  putStrLn . unwords $ map show z
-  putStrLn . unwords $ z'
+  putStrLn . unwords $ map show z -- 1 2 3
+  putStrLn . unwords $ z' -- a b c
