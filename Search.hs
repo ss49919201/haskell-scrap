6d69a7674727cd2a225b7b4ba1b7401e7d851f2b
s-beats <sakashin10291029@gmail.com>
2022-11-19 08:27:13 +0900

    binary search

--- Search.hs
+++ Search.hs
@@ -1,16 +1,19 @@
-main = do
-  solve [1 .. 10] 0 9 19
+main =
+  if binarySearch x 0 (length x -1) 1
+    then putStrLn "Found"
+    else putStrLn "Not found"
+  where
+    x = [1 .. 10]
 
-solve :: [Int] -> Int -> Int -> Int -> IO ()
-solve list low high target = do
-  let mid = (low + high) `div` 2
-  let guess = list !! mid
-  if guess == target
-    then putStrLn "found it"
-    else
-      if low == high
-        then putStrLn "not found"
-        else
-          if guess < target
-            then solve list (mid + 1) high target
-            else solve list low (mid - 1) target
+binarySearch :: [Int] -> Int -> Int -> Int -> Bool
+binarySearch list low high target =
+  (guess == target)
+    || ( low /= high
+           && ( if guess < target
+                  then binarySearch list (mid + 1) high target
+                  else binarySearch list low (mid - 1) target
+              )
+       )
+  where
+    mid = (low + high) `div` 2
+    guess = list !! mid
