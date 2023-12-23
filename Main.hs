712ac22269323e5519c46bd0fd12b12e6ebdaf85
ss49919201 <sakashin10291029@gmail.com>
2023-12-23 18:23:48 +0900

    base

--- Main.hs
+++ Main.hs
@@ -1,18 +1,59 @@
 main = do
   -- 式の途中の負数は括弧で括る
   print (1 * (-3))
+
   -- 論理積、論理和、否定演算子
   print (True && False)
   print (True || False)
   print (not (True))
+
   -- 比較演算子
   print (1 == 1)
   print (1 /= 1)
+
   -- 前置関数
+  -- 関数名、スペース、引数...
   print (min 1 2)
+
   -- 中置関数
   -- 2引数のみ
   print (1 `min` 2)
+
   -- 括弧で括ると順序を制御できる
+  -- Goなら succ(max(2, 3))
   print (succ 2 `max` 3)
   print (succ (2 `max` 3))
+
+  -- 定義した関数の呼び出し
+  print (plus 1 2)
+  print (1 `plus` 2)
+
+  -- if
+  -- else節が必須
+  -- ifは必ず値を返す式
+  let a = 1
+  if a >= 0
+    then print "true"
+    else print "false"
+
+-- 関数定義
+-- 関数名、スペース、引数名...
+plus x y = x + y
+
+-- 引数を取らない関数
+-- 定義、名前と呼ばれる
+name = "Bob"
+
+-- リストは同じ型の要素を格納できる
+names = ["Alice", "Bob"]
+-- リストは`++`で連結できる
+-- 文字列は文字のリストなのでリスト関数を使える
+-- 2つのリストを連結する時、1つ目のリストの最後まで走査されるので、重くなる可能性あり
+concated = names ++ ["Mike"]
+concatedStr = "Hello" ++ "World"
+-- リストの先頭に単一の要素を追加する際は`:`を使う
+-- `:`が、単一の要素と、同じ型のリストを受け取っている中置関数のようであるととらえる
+added = "Mike" : ["Bob"]
+
+-- リストの入れ子は可能だが、型は同じである必要がある
+list = [[1, 2],[3, 4]]
